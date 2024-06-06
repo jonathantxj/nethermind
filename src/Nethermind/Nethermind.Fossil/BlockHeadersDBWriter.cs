dc97258dd2ca683008bc0ad39c92d0d6ac2eaddf
Jonathan Tan <xinjie.tan@thecarousell.com>
2024-06-06 16:08:16 +0800

    change write to binary copy to use single connection

--- src/Nethermind/Nethermind.Fossil/BlockHeadersDBWriter.cs
+++ src/Nethermind/Nethermind.Fossil/BlockHeadersDBWriter.cs
@@ -12,27 +12,6 @@ namespace Nethermind.Fossil
 {
     public class BlockHeadersDBWriter
     {
-        // SETUP DB
-        private static NpgsqlDataSource? _dataSource;
-        private static string connectionString = @"Host=localhost;
-        Port=5432;
-        Database=blockheaders;
-        User Id=jonathantan;
-        Password=;";
-
-        private BlockHeadersDBWriter(NpgsqlDataSource dataSource) {
-            _dataSource = dataSource;
-        }
-
-        async public static Task<BlockHeadersDBWriter> SetupBlockHeadersDBWriter() {
-            NpgsqlDataSource dataSource = NpgsqlDataSource.Create(connectionString);
-            // var dropCommand = dataSource.CreateCommand(DROP_BLOCKHEADERS_TABLE);
-            // await dropCommand.ExecuteNonQueryAsync();
-            var createCommand = dataSource.CreateCommand(CREATE_BLOCKHEADERS_TABLE);
-            await createCommand.ExecuteNonQueryAsync();
-            return new BlockHeadersDBWriter(dataSource);
-        }
-
         private const string DROP_BLOCKHEADERS_TABLE = "DROP TABLE IF EXISTS blockheaders;";
 
         private const string CREATE_BLOCKHEADERS_TABLE = @"
@@ -61,15 +40,31 @@ namespace Nethermind.Fossil
             is_post_merge BOOLEAN NOT NULL,
             total_difficulty VARCHAR(78)
             );";
-        
-        private const string ADD_BLOCKHEADER_IF_NOT_EXISTS = @"
-            INSERT INTO blockheaders
-                VALUES (@hash, @number, @parent_hash, @beneficiary_addr, @gas_limit, @gas_used,
-                @timestamp, @extra_data, @difficulty, @mix_hash, @nonce, @uncles_hash, @tx_root,
-                @receipt_root, @state_root, @bloom, @base_fee_per_gas, @withdrawals_root,
-                @parent_beacon_block_root, @blob_gas_used, @excess_blob_gas, @is_post_merge, @total_difficulty)
-            ON CONFLICT DO NOTHING;";
 
+        private static const string CONNECTION_STRING = @"Host=localhost;
+        Port=5432;
+        Database=blockheaders;
+        User Id=jonathantan;
+        Password=;";
+
+        private static NpgsqlDataSource? _dataSource;
+
+        // SETUP DB
+
+
+        private BlockHeadersDBWriter(NpgsqlDataSource dataSource) {
+            _dataSource = dataSource;
+        }
+
+        async public static Task<BlockHeadersDBWriter> SetupBlockHeadersDBWriter() {
+            NpgsqlDataSource dataSource = NpgsqlDataSource.Create(CONNECTION_STRING);
+            var dropCommand = dataSource.CreateCommand(DROP_BLOCKHEADERS_TABLE);
+            await dropCommand.ExecuteNonQueryAsync();
+            var createCommand = dataSource.CreateCommand(CREATE_BLOCKHEADERS_TABLE);
+            await createCommand.ExecuteNonQueryAsync();
+            return new BlockHeadersDBWriter(dataSource);
+        }
+        
         private string? ULongToHexString(ulong? ul) {
             if (ul == null) {
                 return null;
@@ -77,41 +72,43 @@ namespace Nethermind.Fossil
             return string.Format("0x{0:X4}", ul);
         }
 
-        public async Task WriteToDB(BlockHeader blockHeader)
-        {
-            if (_dataSource == null) return;
-            
-            await using (var conn = await _dataSource.OpenConnectionAsync())
-            await using (var cmd = new NpgsqlCommand(ADD_BLOCKHEADER_IF_NOT_EXISTS, conn))
-            {
-                cmd.Parameters.AddWithValue("hash", (object?) blockHeader.Hash?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("number", blockHeader.Number);
-                cmd.Parameters.AddWithValue("parent_hash", (object?) blockHeader.ParentHash?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("beneficiary_addr", (object?) blockHeader.Beneficiary?.ToString(true) ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("gas_limit", blockHeader.GasLimit);
-                cmd.Parameters.AddWithValue("gas_used", blockHeader.GasUsed);
-                cmd.Parameters.AddWithValue("timestamp", blockHeader.TimestampDate);
-                cmd.Parameters.AddWithValue("extra_data", blockHeader.ExtraData);
-                cmd.Parameters.AddWithValue("difficulty", blockHeader.Difficulty.ToString());
-                cmd.Parameters.AddWithValue("mix_hash", (object?) blockHeader.MixHash?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("nonce", ULongToHexString(blockHeader.Nonce)!);
-                cmd.Parameters.AddWithValue("uncles_hash", (object?) blockHeader.UnclesHash?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("tx_root", (object?) blockHeader.TxRoot?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("receipt_root", (object?) blockHeader.ReceiptsRoot?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("state_root", (object?) blockHeader.StateRoot?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("bloom", (object?) blockHeader.Bloom?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("base_fee_per_gas", blockHeader.BaseFeePerGas.ToString());
-                cmd.Parameters.AddWithValue("withdrawals_root", (object?) blockHeader.WithdrawalsRoot?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("parent_beacon_block_root", (object?) blockHeader.ParentBeaconBlockRoot?.ToString() ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("blob_gas_used", (object?) ULongToHexString(blockHeader.BlobGasUsed) ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("excess_blob_gas", (object?) ULongToHexString(blockHeader.ExcessBlobGas) ?? DBNull.Value);
-                cmd.Parameters.AddWithValue("is_post_merge", blockHeader.IsPostMerge);
-                cmd.Parameters.AddWithValue("total_difficulty", (object?) blockHeader.TotalDifficulty?.ToString() ?? DBNull.Value);
-                
-                await cmd.ExecuteNonQueryAsync();
-                conn.Close();
-            }
-            return;
+        public void WriteBinaryToDB(IEnumerable<BlockHeader?> blockHeaders) {
+
+            using (var conn = _dataSource!.OpenConnection())
+            using (var writer = conn.BeginBinaryImport(
+                "copy blockheaders from STDIN (FORMAT BINARY)"))
+                {
+                    foreach (var blockHeader in blockHeaders)
+                    {
+                        if (blockHeader == null) continue;
+                        writer.StartRow();
+                        writer.Write((object?) blockHeader.Hash?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write(blockHeader.Number, NpgsqlTypes.NpgsqlDbType.Bigint);
+                        writer.Write((object?) blockHeader.ParentHash?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) blockHeader.Beneficiary?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write(blockHeader.GasLimit, NpgsqlTypes.NpgsqlDbType.Bigint);
+                        writer.Write(blockHeader.GasUsed, NpgsqlTypes.NpgsqlDbType.Bigint);
+                        writer.Write(blockHeader.TimestampDate, NpgsqlTypes.NpgsqlDbType.Timestamp);
+                        writer.Write(blockHeader.ExtraData, NpgsqlTypes.NpgsqlDbType.Bytea);
+                        writer.Write(blockHeader.Difficulty.ToString(), NpgsqlTypes.NpgsqlDbType.Varchar);
+                        writer.Write((object?) blockHeader.MixHash?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write(ULongToHexString(blockHeader.Nonce)!, NpgsqlTypes.NpgsqlDbType.Varchar);
+                        writer.Write((object?) blockHeader.UnclesHash?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) blockHeader.TxRoot?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) blockHeader.ReceiptsRoot?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) blockHeader.StateRoot?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) blockHeader.Bloom?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write(blockHeader.BaseFeePerGas.ToString(), NpgsqlTypes.NpgsqlDbType.Varchar);
+                        writer.Write((object?) blockHeader.WithdrawalsRoot?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) blockHeader.ParentBeaconBlockRoot?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Char);
+                        writer.Write((object?) ULongToHexString(blockHeader.BlobGasUsed) ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Varchar);
+                        writer.Write((object?) ULongToHexString(blockHeader.ExcessBlobGas) ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Varchar);
+                        writer.Write( blockHeader.IsPostMerge, NpgsqlTypes.NpgsqlDbType.Boolean);
+                        writer.Write((object?) blockHeader.TotalDifficulty?.ToString() ?? DBNull.Value, NpgsqlTypes.NpgsqlDbType.Varchar);
+                    }
+                    writer.Complete();
+                    conn.Close();
+                }   
         }
     }
 }
--- src/Nethermind/Nethermind.Fossil/FossilPlugin.cs
+++ src/Nethermind/Nethermind.Fossil/FossilPlugin.cs
@@ -16,7 +16,6 @@ namespace Nethermind.Fossil
     public class FossilPlugin : INethermindPlugin
     {
         private INethermindApi _api = null!;
-        // private ReadOnlyBlockTree _readOnlyBlockTree = null!;
         private ILogger _logger;
         private BlockHeadersDBWriter? _dbWriter;
         public virtual string Name => "Fossil";
@@ -29,9 +28,6 @@ namespace Nethermind.Fossil
             _logger = nethermindApi.LogManager.GetClassLogger();
             _dbWriter = BlockHeadersDBWriter.SetupBlockHeadersDBWriter().Result;
 
-            // IBlockTree blockTree = _api.BlockTree ?? throw new ArgumentNullException(nameof(_api.BlockTree));
-            // _readOnlyBlockTree = blockTree.AsReadOnly();
-
             IDb? headersDb = _api.DbProvider?.HeadersDb;
 
             if (headersDb == null)
@@ -40,30 +36,13 @@ namespace Nethermind.Fossil
                     return Task.CompletedTask;
                 }
 
-            int count = 0;
-            int chunkSize = 10000;
-            long headersCount = headersDb.GatherMetric().Size;
-            
-            _logger.Info("Writing " + headersCount + " headers: ");
-
             HeaderDecoder _headerDecoder = new HeaderDecoder();
-            DateTime start = DateTime.Now;
-            foreach (var headersChunk in headersDb.GetAllValues().Chunk(chunkSize))
-                {
-                    var writeTasks = headersChunk
-                    .Select(entry => {
-                        BlockHeader? resultBlockHeader = _headerDecoder.Decode(new RlpStream(entry), RlpBehaviors.None);
-                        if (resultBlockHeader != null) {
-                            return _dbWriter.WriteToDB(resultBlockHeader);
-                        } else {
-                            return Task.CompletedTask;
-                        }
-                    });
-                    Task.WaitAll(writeTasks.ToArray());
-                    count += chunkSize;
-                    _logger.Info(String.Format("Headers Written: {0} -- {1:F2}%", count, (100 * count / headersCount)));
-                }
-                _logger.Info(String.Format("Time Elapsed: {1:g} (h:mm:ss.ffffff)", count, (DateTime.Now - start)));
+
+            var blockHeaders = headersDb.GetAllValues().Select(
+                entry => _headerDecoder.Decode(new RlpStream(entry), RlpBehaviors.None)
+                );
+            _dbWriter.WriteBinaryToDB(blockHeaders);
+
             return Task.CompletedTask;
         }
 
